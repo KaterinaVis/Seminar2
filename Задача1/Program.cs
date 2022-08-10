f76335510922d435322c72be40775a8ac42c04d4
Katerina <katerina4484@mail.ru>
2022-08-10 21:10:02 +0500

    задача1

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа , которая выводит случайное число отрезка [10, 99] и показывает наибольшую цифру этого числа.
+
+int randomNumber = new Random ().Next (10,100); //метод рандом
+
+Console.WriteLine(randomNumber); // вывести случайное число
+
+int firstDigit = randomNumber / 10; // числа у нас двузначные, значит разделить на 10, первое число, остаток от деления на 10 - второе число
+int secondDigit = randomNumber % 10;
+
+if (firstDigit > secondDigit) // условие для вывода большего числа из двух
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
