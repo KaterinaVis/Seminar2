970bda0d186fe40c36a8bcc0e5935164a0abd693
Katerina <katerina4484@mail.ru>
2022-08-11 20:19:12 +0500

    задача2метод

---
+++ Задача2метод/Program.cs
@@ -0,0 +1,20 @@
+﻿/*  Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98     С МЕТОДАМИ */
+
+int randomNumber = GetRandomNumber(100, 999);  // вызов метода рандома
+Console.WriteLine(randomNumber);
+Console.WriteLine(DeleteSecondNumber(randomNumber)); // вызов метода удаления второго числа
+
+int DeleteSecondNumber(int number)  // метод для удаления второго числа
+{
+int firstDigit = randomNumber / 100;
+int lastDigit = randomNumber % 10;
+return firstDigit * 10 + lastDigit;
+}
+
+int GetRandomNumber(int min, int max)  // метод рандома
+{
+return new Random().Next(min, max + 1);
+}
