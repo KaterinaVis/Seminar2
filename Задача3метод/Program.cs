c1d9568689aea646f7f6f6be161275ca175e1625
Katerina <katerina4484@mail.ru>
2022-08-11 20:33:17 +0500

    задача3метод

---
+++ Задача3метод/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму. Если число 1 не кратно числу 2, то программа выводит остаток от деления.
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно     С ИСПОЛЬЗОВАНИЕМ МЕТОДА */
+
+int firstNumber = ReadInt("Первое число: "); // вызываем метод
+int secondNumber = ReadInt("Второе число: "); // вызываем метод
+
+int remainderOfDivision = firstNumber % secondNumber;
+if (remainderOfDivision == 0)
+{
+Console.WriteLine("Числа кратны");
+}
+else
+{
+Console.WriteLine("Числа не кратны, остаток от деления - " + remainderOfDivision);
+}
+
+int ReadInt(string message)  // метод конвертации из ввода в число
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
